3fd08cde559e76dec6818d989f5d8f3569b5c8e0
Luiz Fernando Almeida <luizfernandoalmeidalrv@gmail.com>
2025-09-29 09:48:19 -0400

    Corrigindo Setting Manus para produção 3

--- encantamais-backend/EncantaMais/wsgi.py
+++ encantamais-backend/EncantaMais/wsgi.py
@@ -6,11 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EncantaMais.settings')
 
+print("DEBUG: EncantaMais/wsgi.py está sendo carregado. Tentando obter aplicação WSGI...") # ADICIONE ESTA LINHA
 application = get_wsgi_application()
+print("DEBUG: Aplicação WSGI obtida com sucesso.") # ADICIONE ESTA LINHA
